9709fdc6a9582ccaae29bba0556f7e85db38c9fb
alex-creating <alex-russo1996@hotmail.co.uk>
2019-11-29 09:54:19 +0000

    Finished commit

--- src/conditionals/flowchart.java
+++ src/conditionals/flowchart.java
@@ -1,16 +1,15 @@
 package conditionals;
 
-
-
 public class flowchart {
+public static void main(String [] Args) {
+	
+	int number = 1000;
 
-	
-	int number = 5000;
-	
-	if (number>2000) {
+	if(number>2000)
+	{
 		System.out.println("A"); 
 		if (number>6000) {
-			System.out.println("C");
+			System.out.println("C");}
 		else {
 			System.out.println("B");
 			if (number>4000) {
@@ -21,12 +20,33 @@ public class flowchart {
 			}
 		}
 		}
+
+	else
+
+	{
+		System.out.println("1");
+		if (number > 100) {
+			System.out.println("3");
+		
+		if (number > 600) {
+			System.out.println("5");
+		} else {
+			System.out.println("4");
+		}
+		if (number > 500) {
+			System.out.println("6");
+		} else {
+			System.out.println("7");
+		}}
+	else
+	{
+		System.out.println("2");
+	}	
+		
 	}
+
 	
-	
-	
-	
-	public static void main(String [] Args) {
+
 }
-	
+
 }
